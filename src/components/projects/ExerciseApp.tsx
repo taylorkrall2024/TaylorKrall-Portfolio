b373ba54f2557b42b9a6aa9d7103c1a5a63307d9
taylorkrall1523 <ta505977@ucf.edu>
2024-04-23 13:54:42 -0400

    finaledits

--- src/components/projects/ExerciseApp.tsx
+++ src/components/projects/ExerciseApp.tsx
@@ -6,7 +6,7 @@ export function ExerciseApp(){
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Flash Card App Design </h1>
-            <p class="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"> This assignment is my throughts on a good app for flash card studying called the Study Buddy. The study Buddy fully allows you to design and build your study sets in an organized manner. The blue color gives the user a relazing study sesh without giving it the intense red vibes. </p>
+            <p class="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"> This assignment is my throughts on a good app for flash card studying called the Study Buddy. The Study Buddy fully allows you to design and build your study sets in an organized manner. The blue color gives the user a relaxing study sesh without giving it the intense red vibes. The navigation page is found in the top right corner which allows the user to quickly find any new information they are looking for such as sets, home, new sets, or to edit existing sets.  </p>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
             
