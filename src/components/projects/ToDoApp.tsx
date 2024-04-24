2fa85f196182fb23446c6f37fab2905ff3b91b7a
taylorkrall1523 <ta505977@ucf.edu>
2024-04-24 11:32:55 -0400

    url links

--- src/components/projects/ToDoApp.tsx
+++ src/components/projects/ToDoApp.tsx
@@ -6,7 +6,7 @@ export function TodoApp(){
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">To-Do App</h1>
-            <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A React To-Do app efficiently manages your tasks, allowing seamless adding, updating, and completing of items. The different catagories allow for good organization of the users different work out plans. You are able to add and delete at any time, and add any kind of note or summary that might be needed for your routine. Chose a dark mode for this project to give it more of a mature but respected understanding.  </p>
+            <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A React To-Do app efficiently manages your tasks, allowing seamless adding, updating, and completing of items. The different catagories allow for good organization of the users different work out plans. You are able to add and delete at any time, and add any kind of note or summary that might be needed for your routine. Chose a dark mode for this project to give it more of a mature but respected understanding. https://github.com/taylorkrall2024/taylorkrall/tree/main/lab8  </p>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
         <img src="/projectAssets/gifs/lab8_TaylorKrall.gif" alt="Description of what the GIF shows" />
--- src/components/projects/quizApp.tsx
+++ src/components/projects/quizApp.tsx
@@ -6,7 +6,7 @@ export function QuizApp(){
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Quiz App</h1>
-            <p class="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This React Native quiz app provides a straightforward interface for testing your knowledge. Simple navigation and clear answer choices make for a user-friendly experience. The diferent colors of red and green show the user exactly what is correct and incorrect. The different type of questions are clear when going to pick your response. When creating the new questions, you pick and choose what is correct or incorrect so when the user picks the wrong answer they lose points.  </p>
+            <p class="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This React Native quiz app provides a straightforward interface for testing your knowledge. Simple navigation and clear answer choices make for a user-friendly experience. The diferent colors of red and green show the user exactly what is correct and incorrect. The different type of questions are clear when going to pick your response. When creating the new questions, you pick and choose what is correct or incorrect so when the user picks the wrong answer they lose points. https://github.com/taylorkrall2024/taylorkrall/tree/main/unit2/lab10  </p>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
         <div class="gif-container">
