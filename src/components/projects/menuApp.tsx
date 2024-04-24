e53a95ce2131319c9e2c96d03b0c6acbabc2e7f4
taylorkrall1523 <ta505977@ucf.edu>
2024-04-24 12:18:06 -0400

    summary

--- src/components/projects/menuApp.tsx
+++ src/components/projects/menuApp.tsx
@@ -6,7 +6,7 @@ export function MenuApp(){
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">ToDo Design </h1>
-            <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This was the basis of the ToDo design, where the details were created for the agenda application. Kept the ideas straight forward while organized and gives the user complete access to how they choose to organize their habits. The time is kept for each set in the bottom corner so the user can see how much time they have put into a set.The navigation bar in the top right allows the user to find old or new sets quick.  </p>
+            <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This was the basis of the ToDo design, where the details were created for the agenda application. Kept the ideas straight forward while organized and gives the user complete access to how they choose to organize their habits. The time is kept for each set in the bottom corner so the user can see how much time they have put into a set.The navigation bar in the top right allows the user to find old or new sets quick.   </p>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
         <object data="/projectAssets/images/Ex6_TaylorKrall.pdf" width="800" height="600" type="application/pdf">
